b1fa971109a620110cf6f8df5c14ec559c0ca4e6
gturkoglu <42651056+gturkoglu@users.noreply.github.com>
2021-01-03 17:11:42 +0300

    Update load-magic-link.js

--- src/load-magic-link.js
+++ src/load-magic-link.js
@@ -39,7 +39,7 @@ export default async function loadMagicLink(key) {
                 return;
             }
             
-            sdk = new MagicSDK(key);
+            sdk = new MagicSDK(key, {network: "ropsten"});
             resolve(sdk);
         })
     })
